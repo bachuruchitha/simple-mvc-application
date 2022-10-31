d8fc492edcf977b5b99826d76d3db6578dbdb75c
bachuruchitha <59328374+bachuruchitha@users.noreply.github.com>
2022-10-31 18:39:15 +0530

    Delete src/main directory

--- src/main/java/userlogin/dao/LoginDao.java
+++
@@ -1,7 +0,0 @@
-package userlogin.dao;
-
-public interface LoginDao {
-	boolean validate(String username,String password);
-	public int insertUser(String name,String empid,String username,String phone,String password);
-	public boolean isExist(String username,String empid);
-}
--- src/main/webapp/index.jsp
+++
@@ -1,6 +0,0 @@
-<html>
-<body>
-<h2><a href="http://localhost:8080/userlogin/login">Login</a></h2>
-<h2><a href="http://localhost:8080/userlogin/register">Register</a></h2>
-</body>
-</html>
